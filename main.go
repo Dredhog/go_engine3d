8e1459e9db9310217b0a7e369ef73430a010d478
Lukas Taparauskas <lukastaparauskas@gmail.com>
2016-08-20 03:46:10 +0300

    Switch to a deltaTime framerate independent simulation model.

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"math"
 	"runtime"
-	"time"
 	"training/engine/anim"
 	"training/engine/load/shader"
 	"training/engine/load/texture"
@@ -82,18 +81,15 @@ func main() {
 		modelUniform := gl.GetUniformLocation(shaderProgram, gl.Str("model_mat\x00"))
 		lightPosUniform := gl.GetUniformLocation(shaderProgram, gl.Str("light_position\x00"))
 		boneUniforms := gl.GetUniformLocation(shaderProgram, gl.Str("bone_mat\x00"))
-		_ = boneUniforms
 
 		modelMatrix := mgl32.Ident4()
 		projection := mgl32.Perspective(math.Pi/4, 1.6, 0.1, 100.0)
 		playerPos := mgl32.Vec3{0, 1.2, 6}
 		lightPos := mgl32.Vec3{0, 1, 2}
-		var angle0, angle1, angle2, angle3, angle4, angle5, angle6, angle7 float32
-		t0 := time.Now()
-		startTime := t0
-		frameTime := time.Second / fps
+
 		frames := 0
 		seconds := 0
+
 		firstKeyframe := anim.Keyframe{Ticks: 0, Transforms: []anim.Transform{
 			anim.Transform{[3]float32{1, 1, 1}, [3]float32{}, [3]float32{0, 0, 0}},
 			anim.Transform{[3]float32{1, 1, 1}, [3]float32{}, [3]float32{0, 0, 0}},
@@ -142,24 +138,23 @@ func main() {
 		walkAnimation := anim.Animation{Keyframes: []anim.Keyframe{firstKeyframe, secondKeyframe, thirdKeyframe, fourthKeyframe, fifthKeyframe}}
 		walkAnimation.SetTotalTicks()
 		ticks := float32(0)
+		frameStart := glfw.GetTime()
+		frameTime := 1/float32(fps)
+		deltaTime := frameTime
 
 		for !window.ShouldClose() {
-			//FPS: update, maintain, display
-			frames++
-			time.Sleep(frameTime - time.Since(startTime))
-			if int(time.Since(t0)/time.Second) > seconds {
-				seconds++
-				fmt.Println("fps:", frames)
-				frames = 0
-			}
-			startTime = time.Now()
-
 			//Get input
 			glfw.PollEvents()
-			handleInput(window, &playerPos, &lightPos, &angle0, &angle1, &angle2, &angle3, &angle4, &angle5, &angle6, &angle7)
+			handleInput(window, deltaTime, &playerPos, &lightPos)
+
+			deltaTime = float32(glfw.GetTime() - frameStart)
+			if deltaTime > frameTime{
+				deltaTime = frameTime
+			}
+			frameStart = glfw.GetTime()
 
 			//UPDATE VARIABLES
-			ticks += 50/float32(fps)
+			ticks += 50 * deltaTime
 			camera := mgl32.LookAtV(playerPos, playerPos.Add(mgl32.Vec3{0, -1, -6}), mgl32.Vec3{0, 1, 0})
 			mvpMatrix := projection.Mul4(camera)
 			if err != nil {
@@ -181,8 +176,16 @@ func main() {
 			gl.Uniform3f(lightPosUniform, lightPos[0], lightPos[1], lightPos[2])
 			gl.UniformMatrix4fv(boneUniforms, 8, false, &model.Skeleton.FinalMatrices[0][0])
 
-			gl.UseProgram(shaderProgram)
+			//FPS: update, maintain, display
+			frames++
+			if int(glfw.GetTime()) > seconds {
+				seconds++
+				fmt.Println("fps:", frames)
+				frames = 0
+			}
+
 			//Perform rendering
+			gl.UseProgram(shaderProgram)
 			gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 			model.Mesh.Draw()
 			window.SwapBuffers()
@@ -191,13 +194,11 @@ func main() {
 }
 
 //Input function
-func handleInput(window *glfw.Window, playerPos *mgl32.Vec3, lightPos *mgl32.Vec3, angle0, angle1, angle2, angle3, angle4, angle5, angle6, angle7 *float32) {
-	var navigationSpeed float32 = 5.0 / fps
-	pressedShift := false
+func handleInput(window *glfw.Window, deltaTime float32, playerPos *mgl32.Vec3, lightPos *mgl32.Vec3) {
+	var navigationSpeed float32 = 5.0 * deltaTime
 
-	//Pressing space to exit
-	if window.GetKey(glfw.KeySpace) == glfw.Press ||
-		window.GetKey(glfw.KeyEnter) == glfw.Press {
+	//Pressing space/enter to exit
+	if window.GetKey(glfw.KeySpace) == glfw.Press{
 		window.SetShouldClose(true)
 	}
 	//First person motion
@@ -211,9 +212,6 @@ func handleInput(window *glfw.Window, playerPos *mgl32.Vec3, lightPos *mgl32.Vec
 	} else if window.GetKey(glfw.KeyD) == glfw.Press {
 		playerPos[0] += navigationSpeed
 	}
-	if window.GetKey(glfw.KeyLeftShift) == glfw.Press {
-		pressedShift = true
-	}
 	//light motion
 	if window.GetKey(glfw.KeyUp) == glfw.Press {
 		lightPos[2] -= 5 * navigationSpeed
@@ -224,49 +222,5 @@ func handleInput(window *glfw.Window, playerPos *mgl32.Vec3, lightPos *mgl32.Vec
 		lightPos[0] -= 5 * navigationSpeed
 	} else if window.GetKey(glfw.KeyRight) == glfw.Press {
 		lightPos[0] += 5 * navigationSpeed
-	}
-	//Joint motion
-	if pressedShift {
-		navigationSpeed *= -1
-	}
-	if window.GetKey(glfw.Key0) == glfw.Press {
-		*angle0 += 50 * navigationSpeed
-	}
-	if window.GetKey(glfw.Key1) == glfw.Press {
-		*angle1 += 50 * navigationSpeed
-	}
-	if window.GetKey(glfw.Key2) == glfw.Press {
-		*angle2 += 50 * navigationSpeed
-	}
-	if window.GetKey(glfw.Key3) == glfw.Press {
-		*angle3 += 50 * navigationSpeed
-	}
-	if window.GetKey(glfw.Key4) == glfw.Press {
-		*angle4 += 50 * navigationSpeed
-	}
-	if window.GetKey(glfw.Key5) == glfw.Press {
-		*angle5 += 50 * navigationSpeed
-	}
-	if window.GetKey(glfw.Key6) == glfw.Press {
-		*angle6 += 50 * navigationSpeed
-	}
-	if window.GetKey(glfw.Key7) == glfw.Press {
-		*angle7 += 50 * navigationSpeed
-	}
-	if pressedShift {
-		navigationSpeed *= -1
-	}
-	//Reset Everything
-	if window.GetKey(glfw.KeyR) == glfw.Press {
-		*angle0 = 0
-		*angle1 = 0
-		*angle2 = 0
-		*angle3 = 0
-		*angle4 = 0
-		*angle5 = 0
-		*angle6 = 0
-		*angle7 = 0
-		*angle1 = 0
-		*lightPos = mgl32.Vec3{0, 1, 2}
 	}
 }
